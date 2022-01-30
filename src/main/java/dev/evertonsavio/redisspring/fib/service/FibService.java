437929d0bff8a8b005dcb7cf67f7fceae23e2493
evertonsavio <everluca@outlook.com>
2022-01-30 14:51:53 -0300

    :sparkles: scheduled cache evict

--- src/main/java/dev/evertonsavio/redisspring/fib/service/FibService.java
+++ src/main/java/dev/evertonsavio/redisspring/fib/service/FibService.java
@@ -2,6 +2,7 @@ package dev.evertonsavio.redisspring.fib.service;
 
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,10 +15,16 @@ public class FibService {
         return this.fib(index);
     }
 
-    //PUT POST PATCH DELETE
+    //PUT POST PATCH DELETE TO EVICT
     @CacheEvict(value = "math:fib", key = "#index")
     public void clearCache(int index){
         System.out.println("clearing hash key: " + index);
+    }
+
+    @Scheduled(fixedRate = 10_000)
+    @CacheEvict(value = "math:fib", allEntries = true)
+    public void clearCache(){
+        System.out.println("clearing all fib keys");
     }
 
     private int fib(int index){

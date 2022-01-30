484a0241cf2ae2c3e7c5c140f7c9d07249bc7c39
evertonsavio <everluca@outlook.com>
2022-01-30 13:32:07 -0300

    :tada: spring redis

---
+++ src/test/java/dev/evertonsavio/redisspring/RedisSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.evertonsavio.redisspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

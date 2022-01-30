63b36e3af7cab4720b3c01f502349d78361a95c6
evertonsavio <everluca@outlook.com>
2022-01-30 15:34:20 -0300

    :rocket: caching reactive application

---
+++ src/main/java/dev/evertonsavio/redisspring/city/service/CityService.java
@@ -0,0 +1,38 @@
+package dev.evertonsavio.redisspring.city.service;
+
+import dev.evertonsavio.redisspring.city.client.CityClient;
+import dev.evertonsavio.redisspring.city.dto.City;
+import org.redisson.api.RMapReactive;
+import org.redisson.api.RedissonReactiveClient;
+import org.redisson.codec.TypedJsonJacksonCodec;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+public class CityService {
+
+    @Autowired
+    private CityClient cityClient;
+
+    private final RMapReactive<String, City> cityMap;
+
+    public CityService(RedissonReactiveClient client) {
+        this.cityMap = client.getMap("city", new TypedJsonJacksonCodec(String.class, City.class));
+    }
+
+    public Mono<City> getCity(final String zipCode){
+        return this.cityMap.get(zipCode)
+                .switchIfEmpty(this.cityClient.getCity(zipCode)
+                        .flatMap(city -> this.cityMap.fastPut(zipCode, city).thenReturn(city)));
+    }
+
+    //@Scheduled(fixedRate = 10_000)
+//    public void updateCity(){
+//        this.cityClient.getAll()
+//                .collectList()
+//                .map(list -> list.stream().collect(Collectors.toMap(City::getZip, Function.identity())))
+//                .flatMap(this.cityMap::putAll)
+//                .subscribe();
+//    }
+}
